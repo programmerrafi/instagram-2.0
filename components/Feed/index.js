e9bc9ebff3a2b1cd0e3086d8bbdd08f4cf953572
MD RAFI <79692008+mdrafi10@users.noreply.github.com>
2022-02-14 05:43:15 +0600

    Update index.js

--- components/Feed/index.js
+++ components/Feed/index.js
@@ -9,7 +9,7 @@ function Feed() {
 
   return (
     <main
-      className={`mx-auto grid grid-cols-1 md:max-w-3xl md:grid-cols-2 xl:max-w-6xl xl:grid-cols-3 ${
+      className={`mx-auto grid grid-cols-1 md:max-w-3xl md:grid-cols-2 xl:max-w-4xl xl:grid-cols-3 ${
         !session && "!grid-cols-1 !max-w-3xl"
       }`}
     >
