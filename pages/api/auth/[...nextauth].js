060e2f78de483cb507e75103cd9d72ee9a6d84df
MD RAFI <79692008+mdrafi10@users.noreply.github.com>
2022-02-14 04:57:32 +0600

    Update [...nextauth].js

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -10,17 +10,17 @@ export default NextAuth({
     }),
     // ...add more providers here
   ],
-//   pages: {
-//     signIn: "/auth/signin",
-//   },
-//   callbacks: {
-//     async session({ session, token, user }) {
-//       session.user.username = session.user.name
-//         .split(" ")
-//         .join("")
-//         .toLocaleLowerCase();
-//       session.user.uid = token.sub;
-//       return session;
-//     },
-//   },
+  pages: {
+    signIn: "/auth/signin",
+  },
+  callbacks: {
+    async session({ session, token, user }) {
+      session.user.username = session.user.name
+        .split(" ")
+        .join("")
+        .toLocaleLowerCase();
+      session.user.uid = token.sub;
+      return session;
+    },
+  },
 });
